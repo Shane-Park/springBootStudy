172247539dbd99847f71cc6e6cc8de153b347ab8
Shane Park <psh40963@naver.com>
2021-07-09 21:31:34 +0900

    STS 로 Spring Boot 프로젝트 만들기. 5) 데이터 베이스 사용하기. CRUD 예제

---
+++ firstapp/src/main/java/com/shane/boot/controller/EditTest.java
@@ -0,0 +1,56 @@
+package com.shane.boot.controller;
+
+import javax.transaction.Transactional;
+
+import com.shane.boot.Member;
+import com.shane.boot.repositories.MyRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class EditTest {
+
+	@Autowired
+	MyRepository repository;
+	
+	@GetMapping("/edit/{id}")
+	public ModelAndView editForm(
+		ModelAndView mav
+		,@ModelAttribute("formModel") Member member
+		,@PathVariable long id) {
+
+		mav.setViewName("editTest");
+		member = repository.findById(id);
+		mav.addObject("formModel", member);
+
+		return mav;
+	}
+
+	@PostMapping("/edit")
+	@Transactional
+	public ModelAndView update(
+		@ModelAttribute("formModel") Member member
+		,ModelAndView mav){
+		
+		repository.saveAndFlush(member);
+		return new ModelAndView("redirect:/repository");
+
+	}
+
+	@PostMapping("/delete")
+	@Transactional
+	public ModelAndView remove(@RequestParam long id){
+			repository.deleteById(id);
+			return new ModelAndView("redirect:/repository");
+	}
+
+	
+	
+}
